d0815fb028e527b9bdd7cc9abe72240c5c6200fe
enfeizhang <enfei.zhang@gmail.com>
2026-08-19 15:58:15 +0800

    Refactor setup function to remove unused actions
    Removed loading of actions and action editor during setup.

--- script.js
+++ script.js
@@ -16,9 +16,6 @@ function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   fab = createFab();
 
-  loadActions();
-  initActionEditor();
-
   loadLocalData();
 
   const dataInputs = document.querySelectorAll("input");
